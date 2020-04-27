3aeb35b5cba599506434850befac0c4918733222
paco <paco.paton.antequera@gmail.com>
2020-04-27 15:15:45 +0200

    commit javadoc clase2

--- src/zzzzzESELBUENO/Clase2.java
+++ src/zzzzzESELBUENO/Clase2.java
@@ -2,11 +2,22 @@ package zzzzzESELBUENO;
 
 import java.util.ArrayList;
 
-public class Clase2 {
 
+/**
+ * @author Paco
+ * @version 27/04/2020
+ */
+public class Clase2 {
+	/**
+	 * En el main introducimos la variable ArrayList y resultado, le añadimos 5 numeros, recorremos dicho ArrayList y almacenamos en resultado las variables del arraylist
+	 * Hacemos un metodo que comprobara la media.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
 		ArrayList<Integer> a = new ArrayList<Integer>();
+		
+		
 		
 		a.add(1);
 		a.add(1);
@@ -16,28 +27,35 @@ public class Clase2 {
 		
 		
 		int resultado = 0;
-		int media = 0;
 		
-		for (int i = 0; i < a.size(); i++) {
+		
+		for (int i = 0; i < a.size();  i++) {
 		resultado+=a.get(i) ;
 			
 		}
 		
-		media=resultado / a.size();
-		metodoArrayList(a,media);
-	}
-
-	public static String metodoArrayList(ArrayList<Integer> a, int media) {
-		if(a==null) {
-			return"El arraylist es nulo";
-		}else {
-			if(a.size()==0) {
-				return"El arraylist esta vacio";
-			}else {
-				return"La media de las notas es: "+media;
-			}
 		
+		
+		metodoArrayList(a, resultado);
+	}
+	
+	/**
+	 * Este método funciona de la siguiente forma: le pasamos el ArrayList de la clase main y la variable resultado, realizamos un for para recorrer todas las variables y luego la variable la resultado la dividimos entre la longitud del ArrayList para sacar la media.
+	 * @param a
+	 * @param resultado
+	 * @return
+	 */
+	public static String metodoArrayList(ArrayList<Integer> a,int resultado) {
+		
+		
+	
+		for (int j = 0; j < a.size(); j++) {
+			
 		}
+		return "La media de las notas es: " +resultado/a.size();
+		
+		
+		
 	}
 
 }
